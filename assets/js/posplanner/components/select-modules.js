56801f3acfb235c1729a4c9e539951a1254eb2f0
Smith Milner <smithmilner@gmail.com>
2015-03-01 19:15:10 -0500

    Adding faction filter to module selector. Adding module name sorting.

--- assets/js/posplanner/components/select-modules.js
+++ assets/js/posplanner/components/select-modules.js
@@ -8,25 +8,41 @@ var constants = require('../constants'),
 var SelectModules = React.createClass({
 
   getInitialState: function() {
-    return { value: 'Cruise Missile Battery' };
+    return {
+      value: 'Cruise Missile Battery',
+      faction: false
+    };
   },
 
-  onChange: function(e) {
-    this.setState({ value: e.target.value });
+  toggleFaction: function() {
+    this.setState({ 'faction': !this.state.faction });
   },
 
   addModule: function() {
     dispatcher.dispatch({ type: constants.moduleAdd, content: this.state.value });
   },
 
+  onChange: function(e) {
+    this.setState({ value: e.target.value });
+  },
+
   getOptions: function() {
-    return _.map(data, function (module) {
+    var modules = _.filter(data, function(module) {
+      if (module.meta === 'Faction' && !this.state.faction) { return false; }
+
+      return true;
+    }, this);
+
+    modules = _.sortByAll(modules, ['categories', 'typeName']);
+
+    return _.map(modules, function (module) {
       return <option key={module.typeName} value={module.typeName}>{module.typeName}</option>;
     });
   },
 
   render: function() {
     return <div>
+      <div><a href="#" className="faction-toggle" onClick={this.toggleFaction}>toggle faction</a></div>
       <select value={this.state.value} onChange={this.onChange}>{this.getOptions()}</select>
       <a href="#" className="button" onClick={this.addModule}>add</a>
     </div>;
